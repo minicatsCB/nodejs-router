715a15dc454f1314e7405fc8a911ce104fef7963
Maribel <minicatsCB@users.noreply.github.com>
2019-03-17 13:18:27 +0100

    Use router in server

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const http = require("http");
+const router = require("./router.js");
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+    let template = router.route(req.url);
     res.statusCode = 200;
     res.setHeader("ContentType", "text/plain");
-    res.end("Hello World\n");
+    res.end(template);
 });
 
 server.listen(port, hostname, () => {
